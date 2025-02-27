cedfd0f54f20d6757e4cbc6e59271dfe76e7a96d
MnPhilip <mphilip2@csus.edu>
2025-02-26 18:06:44 -0800

    Updated with info for Matt Philip

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Git!");
-		System.out.println("yang - This line was added by Dr. Yang.");
+		System.out.println("#304056805 - Matt Philip is the Git Master");
 		// Add a new line with your information
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
