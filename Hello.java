a0dd3a36201b242a7a55f71ca507d591dc3d93d0
MnPhilip <mphilip2@csus.edu>
2025-02-28 13:41:19 -0800

    Fixed Formatting

--- Hello.java
+++ Hello.java
@@ -6,16 +6,12 @@ public class Hello {
 		System.out.println("#304056805 - Matt Philip is the Git Master");
 		// Add a new line with your information
 		System.out.println("SacStateID - This line was added by Shivani Bilimoria #304407259.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("302340233 - Nam Nguyen");
-		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("303874792 - This line was added by JD David.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("304272917- This line was added by Saravjot Singh.");
-
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
 }
 
 }
