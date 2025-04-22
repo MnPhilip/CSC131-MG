e51191ed66cbe645beadbf1f0e8493057a211438
MnPhilip <mphilip2@csus.edu>
2025-04-22 00:20:45 +0000

    Same as previous commit

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public static void main(String[] args)
 {
+    RNG generate = new RNG();
+    Monster Monster = new Monster();
+    Player player = new Player();
+
     System.out.println("PROGRAM STARTED SUCCESSFULLY");
-        
+    int tester = generate.diceRoll(4, 4);
+    System.out.println();
+
 }
