067701f5ed8a5df4ed74bbd83e230ee70475c7a4
JD David <extraetc358@gmail.com>
2025-04-26 16:43:06 -0700

    Gave player base hp

--- Monster.java
+++ Monster.java
@@ -7,21 +7,18 @@ class Monster extends Entity
         {
             case 0:
                 // lizard man
-                //fill with stats
                 name = "Lizard Man";
                 hp = 40;
                 dNum = 3;
                 dType = 8;
             case 1:
                 //Air Elemental
-                //fill with stats
                 name = "Air Elemental";
                 hp = 60;
                 dNum = 4;
                 dType = 4;
             case 2:
                 //Fire Elemental
-                //fill with stats
                 name = "Fire Elemental";
                 hp = 60;
                 dNum = 4;
--- Player.java
+++ Player.java
@@ -1,22 +1,23 @@
 public class Player extends Entity {
     //Player names will be sent from GUI when created
     Player(int pRole){
+        hp = 30;
         switch (pRole) {
             case 0: 
             //Wizard, low hp, low amount of dice but high dice type, high mp
-                hp = diceRoll(6, 4);
+                hp += diceRoll(6, 4);
                 hp = generate.diceRoll(12, 2);
                 mp = 20;
                 dNum = 2;
                 dType = 20;
             //Barbarians high hp, low mp, high amount of dice
             case 1:
-                hp = diceRoll(12, 6);
+                hp += diceRoll(12, 6);
                 mp = 10;
                 dNum = 4;
                 dType = 6;
             case 2:
-                hp = diceRoll(10, 4);
+                hp += diceRoll(10, 4);
                 mp = 15;
                 dNum = 6;
                 dType = 4;
