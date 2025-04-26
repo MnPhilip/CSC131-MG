f38949f898cf7467c3fd8c1d5ff8981535a99372
JD David <extraetc358@gmail.com>
2025-04-25 17:02:08 -0700

    little changes to Monster and Entity Classes

--- Entity.java
+++ Entity.java
@@ -21,5 +21,4 @@ public class Entity extends GM{
         return mp;
     }
 
-    public int Status()
 }
--- Monster.java
+++ Monster.java
@@ -2,6 +2,7 @@ class Monster extends Entity
 {
     public Monster(int Mtype)
     {
+        mp = 0;
         switch (Mtype)
         {
             case 0:
@@ -9,7 +10,6 @@ class Monster extends Entity
                 //fill with stats
                 name = "Lizard Man";
                 hp = 40;
-                mp = 0;
                 dNum = 3;
                 dType = 8;
             case 1:
@@ -17,7 +17,6 @@ class Monster extends Entity
                 //fill with stats
                 name = "Air Elemental";
                 hp = 60;
-                mp = 0;
                 dNum = 4;
                 dType = 4;
             case 2:
@@ -25,28 +24,24 @@ class Monster extends Entity
                 //fill with stats
                 name = "Fire Elemental";
                 hp = 60;
-                mp = 0;
                 dNum = 4;
                 dType = 4;
             case 3:
                 //Earth Elemental
                 name = "Earth Elemental";
                 hp = 60;
-                mp = 0;
                 dNum = 4;
                 dType = 4;
             case 4:
                 //Water Elemental
                 name = "Water Elemental";
                 hp = 60;
-                mp = 0;
                 dNum = 4;
                 dType = 4;
             case 5:
                 //Alchemist
                 name = "Alchemist";
                 hp = 80;
-                mp = 0;
                 dNum = 2;
                 dType = 20;
         }
